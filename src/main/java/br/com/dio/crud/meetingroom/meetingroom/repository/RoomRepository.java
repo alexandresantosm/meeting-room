a295fd79aea5f6be849a81db486f67d00345612c
alexandresantosm <amenezes031@gmail.com>
2021-01-18 20:25:56 -0300

    Add class repository

---
+++ src/main/java/br/com/dio/crud/meetingroom/meetingroom/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package br.com.dio.crud.meetingroom.meetingroom.repository;
+
+import br.com.dio.crud.meetingroom.meetingroom.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
