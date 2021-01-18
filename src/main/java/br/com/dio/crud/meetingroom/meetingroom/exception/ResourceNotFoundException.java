fda6b0b1216310dbd3ce80cbe8f7f338db26a646
alexandresantosm <amenezes031@gmail.com>
2021-01-18 20:25:11 -0300

    Add class exceptions

---
+++ src/main/java/br/com/dio/crud/meetingroom/meetingroom/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package br.com.dio.crud.meetingroom.meetingroom.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.function.Supplier;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
